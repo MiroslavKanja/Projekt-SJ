d5ca2545be6d436078597365fb8c8d333b6d78ba
MiroslavKanja <265355576+MiroslavKanja@users.noreply.github.com>
2026-03-03 21:15:39 +0100

    Prvy commit
    toto je prvy commit

--- index.php
+++ index.php
@@ -1,3 +1,17 @@
+<!DOCTYPE html>
+<html lang="sk">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, initial-scale=1.0">
+    <title>Moja prva stranka v PHP</title>
+</head>
+<body>
+<h1>Vitajte na mojej prvej stranke v PHP</h1>
 <?php
-echo "<h1>Hello World</h1>";
+echo "<h1>Henlo World</h1>";
+echo "Dnes je " .date("d.m.Y"). "<br>";
+echo "Aktualnz cas je " .date("H:i:s"). "<br>";
 ?>
+</body>
+</html>
